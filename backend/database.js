b533675299c26bcf1267def1275d27b7d3e953e1
unknown <liyanagethanushadimantha2000@gmail.com>
2022-07-21 22:18:15 -0700

    update new

--- backend/database.js
+++ backend/database.js
@@ -79,7 +79,7 @@ const updateTeacher = async (name, age, id) => {
 }
 
 const deleteTeacher = async (id) => {
-    const sql = "DELETE FROM teacher WHWRE id ="+id
+    const sql = "DELETE FROM teacher WHERE id ="+id
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
@@ -121,7 +121,7 @@ const readStudentInfo = async (id) => {
 }
 
 const addStudent = async (id, name, age, religion) => {
-    const sql = "INSERT INTO student (id, name, age, hometown) VALUES("+'"'+id+'",'+'"'+name+'", '+'"'+age+'",'+'"'+religion+'")'
+    const sql = "INSERT INTO student(id, name, age, hometown) VALUES("+'"'+id+'",'+'"'+name+'", '+'"'+age+'",'+'"'+religion+'")'
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
@@ -135,7 +135,7 @@ const addStudent = async (id, name, age, religion) => {
 }
 
 const updateStudent = async (name, age, religion, id) => {
-    const sql = "UPDATE student SET name="+'"'+name+'"'+','+"age="+'"'+age+'",hometown'+'"'+religion+'"'+"WHERE id="+id
+    const sql = "UPDATE student SET name="+'"'+name+'"'+','+" age="+'"'+age+'", hometown='+'"'+religion+'"'+"WHERE id="+id
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
@@ -149,7 +149,7 @@ const updateStudent = async (name, age, religion, id) => {
 } 
 
 const deleteStudent = async (id) => {
-    const sql = "DELETE * FROM student WHERE id="+id
+    const sql = "DELETE FROM student WHERE id="+id
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
